66437292c1a9f9df32cbdb2beb59bfa7515c2ff0
Dmitriy Sychev <dmitriy.sychev@utexas.edu>
2021-04-08 09:34:16 -0500

    Create post, update post GraphQL

---
+++ src/resolvers/post.ts
@@ -0,0 +1,46 @@
+import { Post } from "../entities/Post";
+import { MyContext } from "../types";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class PostResolver{
+    @Query(() => [Post]) //graphql type
+    posts (@Ctx() {em}: MyContext): Promise<Post[]> { //ts type
+        return em.find(Post, {})
+    }
+
+    @Query(() => Post, {nullable: true}) //graphql type
+    post (
+        @Arg('id', () => Int) id: number, 
+        @Ctx() {em}: MyContext): Promise<Post | null> { //ts type
+        return em.findOne(Post, { id });
+    }
+
+    // mutation for editing data (inserting, deleting, etc)
+    @Mutation(() => Post) //graphql type
+    async createPost (
+        // technically ql can infer the type so specifiying String is not necessary
+        @Arg('title', () => String) title: string, 
+        @Ctx() {em}: MyContext): Promise<Post> { //ts type
+        const post = em.create(Post, {title});
+        await em.persistAndFlush(post);
+        return post;
+    }
+
+    @Mutation(() => Post, {nullable: true}) //graphql type
+    async updatePost (
+        @Arg('id') id: number,
+        // nullable true allows for the title to be null
+        @Arg('title', () => String, {nullable: true}) title: string, 
+        @Ctx() {em}: MyContext): Promise<Post | null> { //ts type
+        const post = await em.findOne(Post, {id});
+        if (!post) {
+            return null;
+        }
+        if (typeof title !== 'undefined') {
+            post.title = title;
+            await em.persistAndFlush(post);
+        }
+        return post;
+    }
+}
